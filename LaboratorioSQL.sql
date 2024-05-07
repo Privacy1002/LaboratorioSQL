94d467e6ff36ff11f835a93e9c63b4a106054cf5
Santiago Parra <124490200+Privacy1002@users.noreply.github.com>
2024-05-07 11:09:08 -0500

    Update LaboratorioSQL.sql
    Se agregan los datos de cada una de las tablas

--- LaboratorioSQL.sql
+++ LaboratorioSQL.sql
@@ -48,7 +48,6 @@ edi_art varchar(300),
 prec_art int,
 primary key (id_art));
 
-
 create table pedido(
 id_ped int auto_increment,
 id_cli_ped varchar(11),
@@ -56,7 +55,6 @@ fec_ped date,
 val_ped int,
 primary key (id_ped),
 foreign key (id_cli_ped) references cliente (id_cli));
-
 
 create table articulo_pedido (
 id_ped_artped int,
@@ -91,7 +89,7 @@ foreign key (autotipo) references tipos_automotores (autotipo));
 create table aseguramientos (
 asecodigo int(6) auto_increment,
 asefechainicio date,
-asefechaexperisacion date ,
+asefechaexperisacion date,
 asevaloraseguramiento int,
 aseestado varchar(20),
 asecosto int ,
@@ -110,60 +108,108 @@ incicautosinvolucrados int,
 primary key (incicodigo),
 foreign key (inciplaca) references automotores (autoplaca));
 
+-- Error Code: 1824. Failed to open the referenced table 'auto_motores'
 
-cod_cur_estcur int auto_increment,
-doc_est_estcur varchar(11),
-fec_ini_estcur date,
+use laboratorio_sql;
 
-insert into profesor ("doc_prof", "nom_prof", "ape_prof", "cate_prof", "sal_prof")
-values (63502720, "martha", "rojas", 2, 690000),
-(91216904, "carlos", "perez", 3, 950000),
-(13826789, "maritza", "angarita", 1, 550000),
-(1098765789, "alejandra", "torres", 4, 1100000);
-
-insert into estudiante_excurso (cod_cur_estcur, doc_est_estcur, fec_ini_estcur)
-values (289011, , "2011-02-01"),
-(250067, 250069, "2011-03-01"),
-(289011, 13826787, "2011-02-01"),
-(345671, 1098764789, "2011-04-01");
-
-insert into estudiante (doc_est, nom_est, ape_est, edad_est)
-values (63502620, "maria", "perez", 2),
-(250069, "carlos jose", "lopez", 3),
-(13826787, "jonatan", "ardila", 1),
-(1098764789, "carlos", "martinez", 4);
-
-DROP TABLE estudiante ;
-
+insert into profesor (doc_prof, nom_prof, ape_prof, cate_prof, sal_prof)
+values 
+(63502720, 'martha', 'rojas', 2, 690000),
+(91216904, 'Carlos', 'Pérez', 3, 950000),
+(13826789, 'Maritza', 'Angarita', 1, 550000),
+(1098765789, 'Alejandra', 'Torres', 4, 1100000);
 
 insert into curso (cod_curs, nom_curs, horas_cur, valor_cur)
-values (149862, "Fundamentos de bases de datos", 40, 500000),
-(250069, "Fundamentos de SQL", 20, 3),
-(13826787, "Manejo de Mysql", 45, 550000),
-(1098764789, "Fundamentals", 60, 3000000);
+values 
+(149842, 'Fundamentos de bases de datos ', 40, 500000),
+(250067, 'Fundamentos de SQL', 20, 700000),
+(289011, 'Manejo de Mysql ', 45 , 550000),
+(345671, 'Fundamentals of Oracle', 60, 3000000);
 
--- Agregar primary key --
+insert into estudiante (doc_est, nom_est, ape_est, edad_est)
+values 
+(63502720, 'María', 'Pérez', 2),
+(91245678, 'Carlos José', 'López', 3),
+(1098098097, 'Jonatan', 'Ardila' , 1),
+(1098765679, 'Carlos', 'Martínez', 4);
 
-ALTER TABLE `LaboratorioSQL`.`Estudiante_excurso` 
-DROP FOREIGN KEY `estudiante_excurso_ibfk_1`;
-ALTER TABLE `LaboratorioSQL`.`Estudiante_excurso` 
-CHANGE COLUMN `doc_est_estcur` `doc_est_estcur` VARCHAR(11) NOT NULL ,
-ADD PRIMARY KEY (`cod_cur_estcur`, `doc_est_estcur`);
-;
-ALTER TABLE `LaboratorioSQL`.`Estudiante_excurso` 
-ADD CONSTRAINT `estudiante_excurso_ibfk_1`
-  FOREIGN KEY (`doc_est_estcur`)
-  REFERENCES `LaboratorioSQL`.`Estudiante` (`doc_est`);
+insert into estudiante_excurso (cod_cur_estcur, doc_est_estcur, fec_ini_estcur)
+values 
+(289011, 1098765679, '2011-02-01'),
+(250067, 63502720, '2011-03-01'),
+(289011, 1098098097, '2011-02-01'),
+(345671, 63502720, '2011-04-01');
 
- 
-SELECT id_ped_artped, id_art_artped, articulo.tit_art, articulo.prec_art, can_art_artped 
-FROM articulo_pedido 
-INNER JOIN articulo 
-ON articulo_pedido.id_art_artped = articulo.id_art
+insert into cliente (id_cli, nom_cli, ape_cli, dir_cli, dep_cli, mes_cum_cli)
+values 
+(63502718, 'Maritza', 'Rojas', 'Calle 34 No.14-45', 'Santander', 'Abril'),
+(13890234, 'Roger', 'Ariza', 'Cra 30 No.13-45', 'Antioquia', 'Junio'),
+(77191956, 'Juan Carlos', 'Arenas', 'Diagonal 23 No. 12-34 apto 101', 'Valle', 'Marzo'),
+(1098765789, 'Catalina', 'Zapata', 'Av. El Libertador No. 30-14', 'Cauca', 'Marzo');
 
- -- Error Code: 1292. Incorrect date value: '-2012' for column 'fec_ini_estcur' at row 1
--- Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`laboratorio_sql`.`estudiante_excurso`, CONSTRAINT `estudiante_excurso_ibfk_1` FOREIGN KEY (`cod_cur_estcur`) REFERENCES `curso` (`cod_curs`))
--- Error Code: 1064. You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'cod_cur_estcur int auto_increment, doc_est_estcur varchar(11), fec_ini_estcur da' at line 1
--- Error Code: 1136. Column count doesn't match value count at row 1
+insert into articulo (tit_art, aut_art, edi_art, prec_art)
+values 
+('Redes cisco', 'Ernesto Arigasello', 'Alfaomega - Rama', 60.000),
+('Facebook y Twitter para adultos', 'Veloso Claudio', 'Alfaomega', 52.000),
+('Creación de un portal con php y mysql', 'Jacob Pavón Puertas', 'Alfaomega - Rama', 40.000),
+('Administración de sistemas operativos', 'Julio Gómez López', 'Alfaomega - Rama', 55.000);
 
--- Error Code: 1824. Failed to open the referenced table 'auto_motores'
+-- Error Code: 1054. Unknown column 'adi_art' in 'field list'
+
+insert into pedido (id_cli_ped, fec_ped, val_ped)
+values 
+(63502718, '2012-02-25', 120000),
+(77191956, '2012-04-30', 55000),
+(63502718, '2011-12-10' , 260000),
+(1098765789, '2012-02-25', 1800000);
+
+insert into articulo_pedido (id_ped_artped, id_art_artped, can_art_artped, val_ven_art_artped)
+values 
+(1, 3 , 5, 40.000),
+(1, 4 , 12, 55.000),
+(2, 1 , 5, 65.000),
+(3, 2 , 10, 55.000),
+(3, 3 , 12, 45.000),
+(4, 1 , 20, 65.000);
+
+insert into company (comnit, comnombre, comyearfun, comeplegal)
+values 
+(800890890-2, 'Seguros Atlantida', 1998, 'Carlos López'),
+(899999999-1, 'Aseguradora Rojas', 1991 , 'Luis Fernando Rojas'),
+(899999999-5, 'Seguros del Estado' , 2001, 'María Margarita Pérez');
+
+-- Error Code: 1146. Table 'laboratorio_sql.companyy' doesn't exist
+-- Error Code: 1054. Unknown column 'id_ped_artped' in 'field list'
+
+alter table tipos_automotores add column autnombre varchar(30) not null;
+
+insert into tipos_automotores (autotipo, autnombre)
+values 
+(1, 'Automóviles'),
+(2, 'Camperos'),
+(3, 'Camiones');
+
+insert into automotores (autoplaca, automarca, autotipo, autmodelo, autopasajeros, autocilindraje, autonumchasis)
+values 
+('FLL420', 'chevrolet corsa', 1, 2003, 5, 1400, 'wywzzz167kk009d25'),
+('DKZ820', 'renault stepway', 1 , 2008, 5, 1600, 'wywwzz157kk009d45'),
+('KJQ920', 'kia sportage' , 2, 2009, 7, 2000, 'wywzzz157kk009d25');
+
+insert into aseguramientos (asefechainicio, asefechaexperisacion, asevaloraseguramiento, aseestado, asecosto, aseplaca)
+values 
+('2012-09-30', '2013-09-30', 30000000, 'Vigente', 500000 , 'FLL420'),
+('2012-09-27', '2013-09-27', 35000000 , 'Vigente', 600000 , 'DKZ820'),
+('2011-09-28', '2012-09-28', 50000000, 'Vigente', 800000 , 'KJQ920');
+
+
+insert into incidentes (incifecha, inciplaca, incilugar, inciantheridos, incianfatalidades, incicautosinvolucrados)
+values 
+('2012-09-30', 'DKZ820', 'Bucaramanga', 0, 0, 2),
+('2012-09-27', 'FLL420', 'Girón', 0, 0, 1),
+('2011-09-28' , 'FLL420', 'Bucaramanga', 1, 0, 2);
+
+
+-- Error Code: 1292. Incorrect date value: '30-09-2012' for column 'asefechainicio' at row 1
+-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ --
+-- Error Code: 1146. Table 'laboratorio_sql.incidententes' doesn't exist
+-- Error Code: 1146. Table 'laboratorio_sql.incidentes' doesn't exist
